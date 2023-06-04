4b03ca57e4b483076f09557bce407483cde8d9ae
Ubuntu <ubuntu@ip-172-31-23-100.ec2.internal>
2023-06-04 16:16:12 +0000

    updating the automation.sh file

--- automation.sh
+++ automation.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-echo "this is an script for bash file"
+echo "this is an script for bash file";
+CHK_1="Naresh-http-log"
+echo $CHK_1
+var2=".tar"
 timestamp=$(date '+%d%m%Y-%H%M%S')
-filename = "Naresh-http-log-"+$timestamp+".tar"
+filename=${CHK_1}$timestamp${var2}
+echo $timestamp
+echo "the filename is $filename"
 
-echo "location is ",$filename
+tar -cvf "./$filename" "/var/log/apache2"
 
-tar -cvf /home/ubuntu/$filename /var/log/apache2
+aws s3 cp "./$filename" "s3://upgrad-naresh"
+size=$("wc --bytes" < "./$filepath")
+echo "size of file $size"
 
-cd..
-
-aws s3 cp /home/ubuntu/$filename s3://upgrad-naresh
-
-#rm $filename
+rm "./$filename"
 
